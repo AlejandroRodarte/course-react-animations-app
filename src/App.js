12141afa09cfe3b2b21b91bd1bbe9f4b0b2396e9
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-04-14 12:56:31 -0600

    Using the Transition Component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Transition } from 'react-transition-group';
 
 import "./App.css";
 import Modal from "./components/Modal/Modal";
@@ -8,7 +9,8 @@ import List from "./components/List/List";
 class App extends Component {
 
   state = {
-    showModal: false
+    showModal: false,
+    showBlock: false
   };
 
   modalClosedHandler = () => this.setState(() => ({ showModal: false }));
@@ -16,15 +18,64 @@ class App extends Component {
   modalOpenedHandler = () => this.setState(() => ({ showModal: true }));
 
   render() {
+
     return (
+
       <div className="App">
-        <h1>React Animations</h1>
+
+        <h1>
+          React Animations
+        </h1>
+
+        <button 
+          className="Button"
+          onClick={ () => this.setState((prevState) => ({ showBlock:!prevState.showBlock })) }
+        >
+          Toggle
+        </button>
+
+        <br />
+
+        <Transition 
+          in={ this.state.showBlock }
+          timeout={ 300 }
+          mountOnEnter
+          unmountOnExit
+        >
+
+          { 
+            (state) => 
+              <div style={{
+                backgroundColor: 'red',
+                width: 100,
+                height: 100,
+                margin: 'auto',
+                transition: 'opacity 1s ease-out',
+                opacity: state === 'exiting' ? 0 : 1
+              }}>
+              </div> 
+          }
+
+        </Transition>
+
         { this.state.showModal && <Modal show closed={ this.modalClosedHandler } /> }
         { this.state.showModal && <Backdrop show closed={ this.modalClosedHandler } /> }
-        <button className="Button" onClick={ this.modalOpenedHandler }>Open Modal</button>
-        <h3>Animating Lists</h3>
+
+        <button 
+          className="Button" 
+          onClick={ this.modalOpenedHandler }
+        >
+          Open Modal
+        </button>
+
+        <h3>
+          Animating Lists
+        </h3>
+
         <List />
+
       </div>
+
     );
   }
 
